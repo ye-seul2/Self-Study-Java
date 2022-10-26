91c1140aac3a347d58f6f7edd97b16df82bb3be6
ye-seul2 <yesle2005@gmail.com>
2022-10-26 18:11:57 +0900

    chap02

---
+++ src/sec04/exam02/KeyCodeExample.java
@@ -0,0 +1,21 @@
+package sec04.exam02;
+
+public class KeyCodeExample {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		int keyCode;
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
+
+	}
+
+}
---
+++ src/sec04/exam03/ContinueKeyCodeReadExample.java
@@ -0,0 +1,17 @@
+package sec04.exam03;
+
+public class ContinueKeyCodeReadExample {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		int keyCode;
+		
+		while(true) {
+			keyCode = System.in.read();
+			System.out.println("keyCode: " + keyCode);
+		}
+
+	}
+
+}
