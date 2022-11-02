dbc474b338194bf590ad716bc8271f22565bfc29
ye-seul2 <yesle2005@gmail.com>
2022-11-02 15:54:01 +0900

    chap03

---
+++ chap03/sec02/exam05/CharOperationExample.java
@@ -0,0 +1,17 @@
+package sec02.exam05;
+
+public class CharOperationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char c1 = 'A' + 1;
+		char c2 = 'A';
+		// char c3 = c2 + 1;
+		System.out.println("c1: " + c1);
+		System.out.println("c2: " + c2);
+		//System.out.println("c3: " + c3);
+
+	}
+
+}
---
+++ chap03/sec02/exam06/StringConcatExample.java
@@ -0,0 +1,19 @@
+package sec02.exam06;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str1 = "JDK" + 6.0;
+		String str2 = str1 + " 특징";
+		System.out.println(str2);
+		
+		String str3 = "JDK" + 3 + 3.0;
+		String str4 = 3 + 3.0 + "JDK";
+		System.out.println(str3);
+		System.out.println(str4);
+
+	}
+
+}
